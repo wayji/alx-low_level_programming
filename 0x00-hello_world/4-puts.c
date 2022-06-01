7234b9c323bdab3c9d18c7d583fcec143c33b816
wayji <yonigebru816@gmail.com>
2022-06-01 14:06:01 -0700

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-   char *s ="\"Programming is like building a multilingual puzzle";
-   puts(s);
-    return (0);
+char *s = "\"Programming is like building a multilingual puzzle";
+puts(s);
+return (0);
 }
