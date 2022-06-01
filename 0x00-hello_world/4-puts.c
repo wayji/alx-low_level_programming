a2b191ef29e5d0bb920ded241080a8be1f8845ca
wayji <yonigebru816@gmail.com>
2022-06-01 13:53:42 -0700

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main() {
-   char *s ="Programming is like building a multilingual puzzle";
+int main()
+{
+   char *s ="\"Programming is like building a multilingual puzzle";
    puts(s);
     return 0;
 }
