588d06696f96a96d0673526a516bf437c52ada67
wayji <yonigebru816@gmail.com>
2022-06-03 16:21:40 -0700

    ih

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,13 @@
 *
 * Return: Always 0 (Success)
 */
-int main()
+int main(void)
 {
 char i;
-for(i='a'; i<='z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 putchar(i);
 }
 putchar('\n');
-return 0;
+return (0);
 }
