f6bf297c48ee66cb6e60b975ae6d4cbdd401caf8
wayji <yonigebru816@gmail.com>
2022-06-04 02:54:32 -0700

    yg

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and is 0\n", n, las);
 }
 if (las < 6 && las != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n",n , las);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, las);
 }
 return (0);
 }
