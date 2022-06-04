b0b2d3b7dc06872afb6fd6db037452f520fd9d6a
wayji <yonigebru816@gmail.com>
2022-06-04 03:54:08 -0700

    yg

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char i;
+i ='a';
+while(i<='z')
+{
+if (i !='e' && i !='q')
+{
+putchar(i);
+}
+i++;
+}
+putchar('\n');
+return(0);
+}
