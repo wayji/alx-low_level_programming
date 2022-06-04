6f9e071ff5f018bff35c1decad49ceb180db6680
wayji <yonigebru816@gmail.com>
2022-06-03 17:00:47 -0700

    hi

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,8 @@ if (las == 0)
 printf("Last digit of %d is %d and is 0\n", n, las);
 }                                                                     if (las < 6 && las != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, las);
+printf("Last digit of %d is %d and is less than 6 and not
+	       	0\n", n, las);
 }
 return (0);
 }
