5cf1b9327e4f1070b5793b43b3a90c1597de294d
wayji <yonigebru816@gmail.com>
2022-06-05 09:32:09 -0700

    jgj

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void) 
+{
+int i;
+int g;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (g ='a'; g <='f' ; g++ )
+{
+putchar(g);
+}
+return (0);
+}
