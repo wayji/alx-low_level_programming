40d8764ca1f0dd5597b6cb0d438f1824d8286f1a
wayji <yonigebru816@gmail.com>
2022-06-05 00:42:13 -0700

    hi

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-/*ifgjkg
- * hhfh
- * ccc
- */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i;
 i = 0;
-while ( i < 10 )
+while (i < 10)
 {
 printf("%d", i);
 i++;
 }
+putchar('\n');
 return (0);
 }
