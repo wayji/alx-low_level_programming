9710404b972b1f3faf3b5cf7934ced3eead7d9f1
wayji <yonigebru816@gmail.com>
2022-06-07 12:10:09 -0700

    oh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main() {
-printf("_putchar");
-return 0;
+/**
+* main - Prints numbers between 00 to 99.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("_putchar\n");
+return ()0;
 }
