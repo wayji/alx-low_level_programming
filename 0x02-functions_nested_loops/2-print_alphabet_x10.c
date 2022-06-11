00a8339601cad177d7236fc1a5f459d989c974f0
wayji <yonigebru816@gmail.com>
2022-06-11 05:13:58 -0700

    oh

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int r;
+r = 0;
+char letter = 'a';
+while (r <= 9)
+{
+while (letter <= 'z') 
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+r++;
+}
+}
