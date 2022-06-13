51f166cfad394ce098ffcdab5776640afa07282f
wayji <yonigebru816@gmail.com>
2022-06-13 11:31:31 -0700

    oh

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer and updates the value it point to
+ * @n: pointer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
