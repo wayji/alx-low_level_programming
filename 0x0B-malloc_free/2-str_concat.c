da7274b3a14af7c75bdf7a697b44bec9b1cbf818
wayji <yonigebru816@gmail.com>
2022-06-28 05:49:00 -0700

    oh

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,8 +10,8 @@
  */
 char *str_concat(char *s1, char *s2)
 {
- char *strout;
- unsigned int i, j, k, limit;
+char *strout;
+unsigned int i, j, k, limit;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
