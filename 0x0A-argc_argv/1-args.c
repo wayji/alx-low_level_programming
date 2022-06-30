8aa110745816bf48335a42f5dbaae27ae82f7e60
wayji <yonigebru816@gmail.com>
2022-06-29 23:36:36 -0700

    oh

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
